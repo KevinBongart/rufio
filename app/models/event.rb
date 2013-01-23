2a8b4a3c3960cb80f2d7af7c210959e6aa496c75
Kevin Bongart <contact@kevinbongart.net>
2013-01-23 14:56:21 -0500

    Order events.

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,5 @@
 class Event < ActiveRecord::Base
   attr_accessible :campaign_id, :campaign_name, :customer_id, :email_address, :email_id, :event_id, :event_type, :subject, :timestamp
+
+  default_scope order('created_at DESC')
 end
