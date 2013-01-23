f1b5fec72ff7677a7a9e953e96271f14ec5f1da3
Kevin Bongart <contact@kevinbongart.net>
2013-01-23 12:58:00 -0500

    Base app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rufio::Application.config.secret_token = 'a5c27f65e263709594c5ed725c7efa40fbf548eca0a806eeaaba790184d64e1c3e23f5c3c132b1062b0ce70e5b2cbba1b68f283ab67a25045de338d6000f6d91'
