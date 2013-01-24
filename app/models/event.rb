90f95cb49bd18b7e57b785015a38c4932e203d21
Kevin Bongart <contact@kevinbongart.net>
2013-01-24 11:12:34 -0500

    Validates uniqueness of :event_id.

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,6 @@
 class Event < ActiveRecord::Base
   attr_accessible :campaign_id, :campaign_name, :customer_id, :email_address, :email_id, :event_id, :event_type, :subject, :timestamp, :environment_id
+  validates_uniqueness_of :event_id
 
   belongs_to :environment
 
